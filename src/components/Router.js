ee3463a94ba060f8fc52cfb587372b7e5116ab77
Scope0204 <scope0204@gmail.com>
2021-12-19 22:41:39 +0900

    firebase setting

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Routes>
