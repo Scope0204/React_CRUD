c9d94971f091755a817bbb57468934eeb089dcc6
Scope0204 <scope0204@gmail.com>
2021-12-20 15:30:56 +0900

    firebase auth v9

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (event) => {
+    console.log(event.target.name);
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const auth = getAuth();
+    let data;
+    try {
+      if (newAccount) {
+        data = createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        data = signInWithEmailAndPassword(auth, email, password);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Github </button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
