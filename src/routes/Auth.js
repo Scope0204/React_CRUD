09ee04597feb6c7063a8c72b540221bc1f826dac
Scope0204 <scope0204@gmail.com>
2021-12-21 16:37:47 +0900

    social login, logout

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  signInWithPopup,
+  GithubAuthProvider,
+  GoogleAuthProvider,
 } from "firebase/auth";
 
 const Auth = () => {
@@ -10,6 +13,7 @@ const Auth = () => {
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
   const [error, setError] = useState("");
+  const auth = getAuth();
 
   const onChange = (event) => {
     console.log(event.target.name);
@@ -25,16 +29,13 @@ const Auth = () => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-
     try {
-      const auth = getAuth();
       let data;
       if (newAccount) {
         data = await createUserWithEmailAndPassword(auth, email, password);
       } else {
         data = await signInWithEmailAndPassword(auth, email, password);
       }
-      // console.log(data);
     } catch (error) {
       console.log(error.message);
       setError(error.message);
@@ -42,7 +43,29 @@ const Auth = () => {
   };
 
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name }, //es6
+    } = event;
 
+    let provider, result, credential, token; // credential :
+    try {
+      if (name === "google") {
+        provider = new GoogleAuthProvider();
+        result = await signInWithPopup(auth, provider);
+        credential = GoogleAuthProvider.credentialFromResult(result);
+        token = credential.accessToken;
+      } else if (name === "github") {
+        provider = new GithubAuthProvider();
+        result = await signInWithPopup(auth, provider);
+        credential = GithubAuthProvider.credentialFromResult(result);
+        token = credential.accessToken;
+      }
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -69,7 +92,12 @@ const Auth = () => {
         {newAccount ? "Sign in" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Github </button>
+        <button onClick={onSocialClick} name="Google">
+          Continue with Google{" "}
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github{" "}
+        </button>
       </div>
     </div>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
-export default Profile;
+export default () => {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    signOut(auth);
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
