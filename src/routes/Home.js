60c7abae79402f9173acb2b546f46b2b1588c731
Scope0204 <scope0204@gmail.com>
2021-12-22 14:20:29 +0900

    firestore connect

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../fbase";
+import { addDoc, collection } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createAt: Date.now(),
+      });
+      console.log("document written with ID :", docRef.id);
+    } catch (err) {
+      console.log(err);
+    }
+    setNweet(""); //등록 시 입력폼에 값 초기화
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          type="text"
+          placeholder="무엇을 할까요"
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" value="NWeet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
